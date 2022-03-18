3eae44f73a590d12ef36c60618bc39048a5d9666
bcqsd <20204248@cqu.edu.cn>
2022-03-18 21:11:22 +0800

    finish reactive Object

--- reactive/Observer.js
+++ reactive/Observer.js
@@ -1,5 +1,19 @@
 import Dep from './Dep'
+export class Observer{
+    constructor(value){
+        this.value=value
+        if(!Array.isArray(value)){
+            this.walk(value)
+        }
+    }
+    walk(obj){
+        Object.keys(obj).forEach(p=>defineReactive(obj,p,obj[p]))
+    }
+}
 function defineReactive(data,key,val){
+    if(typeof val === 'object'){
+        new Observer(val)
+    }
     let dep=new Dep()
     Object.defineProperty(data,key,{
         enumerable:true,
