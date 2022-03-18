71e01058397d84b0a686b2bd47530728af9a33ac
bcqsd <20204248@cqu.edu.cn>
2022-03-18 18:05:45 +0800

    执行get方法会不会导致watcher被dep数组反复添加

--- reactive/Watcher.js
+++ reactive/Watcher.js
@@ -6,10 +6,11 @@ export default class Watcher{
         this.vm=vm
         this.getter=parsePath(expOrFn)
         this.cb=cb
-        //获取的响应式数据
+        //获取响应式数据,并在读取响应式数据的同时把自己加到数据的Dep数组里,同时将旧数据缓存到value属性上
         this.value=this.get()
     }
     get(){
+        //?每次读取数据会不会导致dep数组里有重复元素
         window.target=this
         //读取data的属性,触发getter收集位于window.target上的依赖
         let value=this.getter(this.vm) 
