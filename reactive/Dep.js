102b6e8dd49a15245e53d1c7146f68f911dd4c08
bcqsd <20204248@cqu.edu.cn>
2022-03-21 20:51:41 +0800

    watcher has a dep array

--- reactive/Dep.js
+++ reactive/Dep.js
@@ -1,10 +1,14 @@
+let uid=0
 export default class Dep{
    constructor(){
+       //每个依赖列表的唯一标识
+       this.id=uid++
        //依赖数组
        this.subs=[]
    }
    addSub(){
-      window.target&&this.subs.push(window.target)
+       //让位于window.target上的watcher对象自己加到数组里来
+      window.target&&window.target.addDep(this)
    }
    removeSub(sub){
         const index=this.subs.indexOf(sub)
--- reactive/Watcher.js
+++ reactive/Watcher.js
@@ -1,13 +1,23 @@
 import { parsePath } from "./utils"
-
+/**
+ * expOrFn  (obj)=>obj.b.c or  b.c
+ */
 export default class Watcher{
     constructor(vm,expOrFn,cb){
         //将dom节点绑定到实例属性上
         this.vm=vm
-        this.getter=parsePath(expOrFn)
+        if(typeof expOrFn ==='function'){
+              this.getter=expOrFn
+        }
+        else{
+            this.getter=parsePath(expOrFn)
+        }
         this.cb=cb
         //获取响应式数据,并在读取响应式数据的同时把自己加到数据的Dep数组里,同时将旧数据缓存到value属性上
         this.value=this.get()
+        //watcher与dep是多对多的关系，当expOfFn是函数时，一个watcher就要收集多个Dep
+        this.deps=[]
+        this.depIds=new Set()
     }
     get(){
         //?每次读取数据会不会导致dep数组里有重复元素
@@ -21,5 +31,14 @@ export default class Watcher{
         const oldValue=this.value
         this.value=this.get()
         this.cb.call(this.vm,this.value,oldValue)
+    }
+    //判断是否已订阅dep，如果没有，订阅并加到已订阅列表
+    addDep(dep){
+        const id=dep.id
+        if(!this.depIds.has(id)){
+            this.depIds.add(id)
+            this.deps.push(dep)
+            dep.addSub(this)
+        }
     }
 }
