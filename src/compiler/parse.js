bbc5519e5d731d342e453e4a464d39a78e117e0a
bcqsd <20204248@cqu.edu.cn>
2022-04-14 12:43:16 +0800

    feat:parse init

--- src/compiler/parse.js
+++ src/compiler/parse.js
@@ -29,7 +29,29 @@ function parseChildren(context){
         }     
         nodes.push(node)
     }
-    return nodes
+    let removedWhitedspaces=false
+    for(let i=0;i<nodes.length;++i){
+        const node=nodes[i]
+        if(node.type===NodeTypes.TEXT){
+            if(/[^\t\r\f\n ]/.test(node.content)){
+                node.content=node.content.replace(/[^\t\r\f\n ]+/g,' ')
+            }else{
+                const prev=nodes[i-1]
+                const next=nodes[i+1]
+                if(!prev||!next||
+                    (prev.type==NodeTypes.ELEMENT &&next.type===NodeTypes.ELEMENT
+                      &&/[\r\n]/.test(node.content))
+                 ){
+                     //删除空白节点
+                     nodes[i]=null  
+                     removedWhitedspaces=true
+                  }else{
+                      node.content=' '
+                  }  
+            }
+        }
+    }
+    return removedWhitedspaces?nodes:nodes.filter(Boolean)
 }
 //缺点：不能解析a<b
 function parseText(context){
@@ -42,7 +64,7 @@ function parseText(context){
             endIndex=index
         }
     }
-   const content=parseTextData(context,length)
+   const content=parseTextData(context,endIndex)
    return {
        type:NodeTypes.TEXT,
        content
@@ -55,11 +77,95 @@ function parseTextData(context,length){
 }
 
 function parseInterpolation(context){
-
+    const [open,close]=context.options.delimiters
+    advanceBy(context,open.length)
+    const closeIndex=context.source.indexOf(close)
+    const content=parseTextData(context,closeIndex).trim()
+    advanceBy(context.close.length)
+    return {
+        type:NodeTypes.INTERPOLATION,
+        content:{
+            type:NodeTypes.SIMPLE_EXPRESSION,
+            content,
+            isStatic:false
+        }
+    }
 }
 
 function parseElement(context){
+   const element=parseTag(context)
+   if(element.isSelfClosing) {
+       return element
+   }
+   element.children=parseChildren(context)
+   parseTag(context)
+   return element
+}
 
+function parseTag(context){
+    const match=/^<\/?([a-z][^\t\r\n\f />]*)/i.exec(context.source)
+    const tag=match[1]
+    advanceBy(context,match[0].length)
+    advanceSpaces(context)
+    const {props}=parseAttributes(context)
+    const isSelfClosing=context.source.startsWith('/>')
+    advanceBy(context,isSelfClosing?2:1)
+    const tagType=isComponent(tag)?ELementTypes.COMPONENT:ELementTypes.ELEMENT
+    return {
+        type:NodeTypes.ELEMENT,
+        tag,
+        tagType,
+        props,
+        isSelfClosing,
+        children:[]
+    }
+}
+
+function isComponent(tag){
+    //首字母大写是组件
+    const reg=/^[A-Z]+/
+    return reg.exec(tag)
+}
+//解析属性
+function parseAttributes(context){
+   const props=[]
+   while(context.source.length || context.source.startsWith('>')
+    ||context.source.startsWith('/>')){
+       let attr=parseAttribute(context)
+       props.push(attr)
+   }
+   return props
+}
+//解析属性名
+function parseAttribute(context){
+       const match=/^[^\t\r\n\f ][^\t\r\n\f />=]*/.exec(context.source)
+       const name=match[0]
+       advanceBy(context,name.length)
+       advanceSpaces(context)
+       let value;
+       if(context.source[0]==='='){
+           advanceBy(context,1)
+           advanceSpaces(context)
+           value=parseAttributeValue(context)
+       }
+       return {
+           type:NodeTypes.ATTRIBUTE,
+           name,
+           value:value && {
+               type:NodeTypes.TEXT,
+               content:value.content
+           }
+       }
+}
+//解析属性值
+function parseAttributeValue(context){
+    const quote=context[0]
+    advanceBy(context,1)
+    const endIndex=context.source.indexOf(quote)
+    const content=parseTextData(context,endIndex)
+     advanceBy(context,1)
+     advanceSpaces(context)
+    return content
 }
 
 function isEnd(context){
@@ -78,3 +184,4 @@ function advanceSpaces(context){
         advanceBy(context,match[0].length)
     }     
 }
+
