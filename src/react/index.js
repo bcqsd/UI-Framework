4220a49d3a613008cea0a1c151c633ed4079e697
bcqsd <20204248@cqu.edu.cn>
2022-04-20 10:48:44 +0800

    fiber render

--- src/react/index.js
+++ src/react/index.js
@@ -1,15 +1,12 @@
-function createElement(type,props,...children){
-    return {
-        type,
-        props:{
-            ...props,
-            children:children.map(child=>
-                typeof child==='object'
-                ?child:
-                createTextElement(child)
-            )
-        }
-    }
+function createDom(fiber){
+ const dom=fiber.type==='TEXT_ELEMENT'
+ ? document.createTextNode("")
+ :document.createElement(fiber.type)
+ 
+ const isProperty=key=>key!=='children'
+ Object.keys(fiber.props).filter(isProperty).forEach(key=>dom[key]=fiber.props[key])
+
+ return dom
 }
 
 function createTextElement(text){
@@ -21,17 +18,15 @@ function createTextElement(text){
         }
     }
 }
+let nextUnitOfWork=null
 
 function render(element,container){
-    const dom=element.type==='TEXT_ELEMENT'
-    ? document.createTextNode('')
-    :document.createElement(element.type)
-    element.props.children.forEach(child=>render(child,dom))
-
-    //挂载属性和方法
-    const isProperty=key=>key!=='children'&&!/^on[A-Z].*/.test(key)
-    Object.keys(element.props).filter(isProperty).forEach(key=>dom[key]=element.props[key])
-    container.appendChild(dom)
+    nextUnitOfWork= {
+        dom:container,
+        props:{
+            children:[element]
+        }
+    }
 }
 
 const Didact={
@@ -45,6 +40,63 @@ const element=(
           <b/>
     </div>
 )
+
+
+function workLoop(deadline){
+    let shouldYield=false
+    //在有任务且时间充足的情况下不断执行
+    while(nextUnitOfWork &&!shouldYield){
+        nextUnitOfWork=perfromUnitOfWork(nextUnitOfWork)
+        shouldYield=deadline.timeRemaining()<1
+    }
+    //把控制权还给浏览器
+    requestIdleCallback(workLoop)
+}
+requestIdleCallback(workLoop)
+
+function perfromUnitOfWork(fiber){
+    //创建fiber节点对应的dom树
+   if(!fiber.dom) {
+       fiber.dom=createDom(fiber)
+   }
+   if(fiber.parent){
+       fiber.parent.dom.appendChild(fiber.dom)
+   }
+   //创建该fiber节点对应的children fiber节点
+   const elements=fiber.props.children
+   let index=0
+   let prevSibling=null
+
+   while(index<elements.length){
+       const element=elements[index]
+       const newFiber={
+           type:element.type,
+           props:element.props,
+           parent:fiber,
+           dom:null
+       }
+       if(index==0){
+           fiber.child=newFiber
+       }else{
+           prevSibling.sibing=newFiber
+       }
+       prevSibling=newFiber
+       index++
+   }
+   //执行渲染
+   if(fiber.child){
+       return fiber.child
+   } 
+   let nextFiber=fiber
+   while(nextFiber){
+       if(nextFiber.sibing){
+           return nextFiber.sibing
+       }
+       nextFiber=nextFiber.parent
+   }
+}
+
+
 
 const container=document.getElementById('root')
 Didact.render(element,container)
